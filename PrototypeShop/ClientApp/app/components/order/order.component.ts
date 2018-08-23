6bdcdbe25915b8b1191c61ffe3e713b8c5123690
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2018-08-23 16:36:57 +0200

    cleaned up OrderComponent

--- PrototypeShop/ClientApp/app/components/order/order.component.ts
+++ PrototypeShop/ClientApp/app/components/order/order.component.ts
@@ -1,31 +1,8 @@
-import { Component, Inject, OnInit, OnDestroy, ComponentFactoryResolver, ViewChild } from '@angular/core';
-import { Http, RequestOptions, Headers } from '@angular/http';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'order',
     templateUrl: './order.component.html'
 })
-export class OrderComponent implements OnInit {
-    private http: Http;
-    private baseUrl: string;
-    private paymentToken: string;
-
-
-    constructor(http: Http, @Inject('BASE_URL') baseUrl: string, private componentFactoryResolver: ComponentFactoryResolver) {
-        this.http = http;
-        this.baseUrl = baseUrl;
-    }
-
-    ngOnInit() {
-        let headers: Headers = new Headers({ 'Content-Type': 'application/json' });
-        let requestOptions: RequestOptions = new RequestOptions();
-        requestOptions.headers = headers;
-        this.http.post(this.baseUrl + 'api/Checkout/GetPaymentToken', '{"value": "300"}', requestOptions).subscribe(
-            result => {
-                this.paymentToken = result.text() as string;
-                console.log(this.paymentToken);
-            },
-            error => console.error(error)
-        )
-    }
+export class OrderComponent {
 }
