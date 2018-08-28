92f6d981684a606f4c3499960d5f6cce8298c061
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2018-08-28 11:33:16 +0200

    updated cart lifecycle for getPaymentToken()

--- PrototypeShop/ClientApp/app/components/cart/cart.component.ts
+++ PrototypeShop/ClientApp/app/components/cart/cart.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, Inject, OnDestroy } from '@angular/core';
+import { Component, Input, Inject, OnDestroy, OnInit } from '@angular/core';
 import { Http, RequestOptions, Headers } from '@angular/http';
 import { CheckoutSummaryService } from '../../services/checkoutsummary.service';
 import { Cart } from '../cart/cart';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -10,38 +11,51 @@ import { Cart } from '../cart/cart';
     styleUrls: ['./cart.component.css']
 })
 
-export class CartComponent implements OnDestroy {
+export class CartComponent implements OnInit, OnDestroy {
+    private subscriptions: Subscription[] = [];
     public cart: Cart | undefined;
     @Input() isSummary: boolean = true;
 
-    constructor(private http: Http, @Inject('BASE_URL') private baseUrl: string, private checkoutSummaryService: CheckoutSummaryService) {
+    constructor(private http: Http, @Inject('BASE_URL') private baseUrl: string, private checkoutSummaryService: CheckoutSummaryService) { }
+
+    ngOnInit() {
         this.makeSubscriptions();
     }
 
     ngOnDestroy() {
-        let cart = this.cart as Cart
+        if (this.isSummary) {
+            this.getPaymentToken();
+        }
+        
+        this.subscriptions.forEach(subscription => subscription.unsubscribe());
+    }
+
+    private makeSubscriptions() {
+        let cartSubscription: Subscription = this.checkoutSummaryService.cart$.subscribe(
+            cart => {
+                this.cart = cart;
+            }
+        )
+        this.subscriptions.push(cartSubscription);
+    }
+
+    private getPaymentToken() {
+        let cart = this.cart as Cart;
         let headers: Headers = new Headers({ 'Content-Type': 'application/json' });
         let requestOptions: RequestOptions = new RequestOptions();
         requestOptions.headers = headers;
         let payload = {
             "value": cart.total,
             "currency": cart.currency
-        }
-        this.http.post(this.baseUrl + 'api/Checkout/GetPaymentToken', payload, requestOptions).subscribe(
-            result => {
-                let paymentToken = result.text() as string;
-                console.log(paymentToken);
-                this.checkoutSummaryService.updatePaymentToken(paymentToken);
-            },
-            error => console.error(error)
+        };
+        this.http.post(this.baseUrl + 'api/Checkout/GetPaymentToken', payload, requestOptions)
+            .subscribe(
+                result => {
+                    let paymentToken = result.text() as string;
+                    console.log(paymentToken);
+                    this.checkoutSummaryService.updatePaymentToken(paymentToken);
+                },
+                error => console.error(error)
         );
-    }
-
-    private makeSubscriptions() {
-        this.checkoutSummaryService.cart$.subscribe(
-            cart => {
-                this.cart = cart;
-            }
-        )
     }
 }
