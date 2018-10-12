9cab03154a4650aa3df31e2345d1bc26a306602f
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2018-10-12 09:26:07 +0200

    removed obsolete reference to navmenu.component.css

--- PrototypeShop/ClientApp/app/components/navmenu/navmenu.component.ts
+++ PrototypeShop/ClientApp/app/components/navmenu/navmenu.component.ts
@@ -3,8 +3,7 @@ import { Customer } from '../../classes/customer/customer';
 
 @Component({
     selector: 'nav-menu',
-    templateUrl: './navmenu.component.html',
-    styleUrls: ['./navmenu.component.css']
+    templateUrl: './navmenu.component.html'
 })
 export class NavMenuComponent {
     @Input() customer: Customer;
